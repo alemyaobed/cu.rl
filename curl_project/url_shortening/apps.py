bca800bc1f12cf4edbef83e397a1b0030050f960
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-04-23 23:04:35 +0000

    some minor folder renaming

--- curl_project/url_shortening/apps.py
+++ curl_project/url_shortening/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UrlsConfig(AppConfig):
+class UrlShorteningConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'urls'
+    name = 'url_shortening'
